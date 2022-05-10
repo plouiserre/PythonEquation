0f78e46af47a3fa74dda203b3b373af99128b78a
Pierre-Louis Serré <plouiserre@gmail.com>
2022-05-10 07:40:29 +0200

    add check of  */ not enchained and = good placement in equation

--- analyze.py
+++ analyze.py
@@ -5,8 +5,39 @@ class Analyze :
 
     def IsValidate(self) :
         isOk = True
-        for element in self.Equation : 
+        isEqualSignOk = False
+        for index, element in enumerate(self.Equation) : 
             isAlpha = element.isalpha()
             if isAlpha  and element != 'x' :
                 isOk = False
+            elif index > 0 and ((element == '/' and self.Equation[index - 1] == '*') or (element == '*' and self.Equation[index - 1] == '/')):
+                isOk = False
+            elif self.EqualSignIsValidate(element,index) :
+                isEqualSignOk = True
+
+        if isOk and isEqualSignOk == False :
+            isOk = False 
+        elif isOk and self.UnknowInLeftPart() == False :
+            isOk = False
+
         return isOk
+
+
+    def EqualSignIsValidate(self, element, index) : 
+        isEqualSignOk = False 
+        if element == '=' and index > 0 and index < len(self.Equation) - 1 :
+            lastElement = self.Equation[index - 1]
+            if lastElement != '+' and lastElement != '-' and lastElement != '*' and lastElement != '/' :
+                isEqualSignOk = True
+        return isEqualSignOk
+        
+
+    def UnknowInLeftPart(self) : 
+        parts = self.Equation.split("=")
+        IsUnknowInLeftPart = False
+
+        for element in parts[0] :
+            if element == 'x' : 
+                IsUnknowInLeftPart = True
+
+        return IsUnknowInLeftPart
--- test_analyze.py
+++ test_analyze.py
@@ -20,6 +20,70 @@ class AnalyseTestTest(unittest.TestCase) :
         isOk = analyze.IsValidate()
         
         self.assertFalse(isOk)
+
     
-    '''def test_multiplication_division_separated(self) :
-        equation = "09" '''
+    def test_multiplication_division_separated(self) :
+        equation = "09x*/2=67" 
+        analyze = Analyze(equation)
+
+        isOk = analyze.IsValidate()
+
+        self.assertFalse(isOk)
+
+
+    def test_division_multiplication_separated(self) :
+        equation = "09x/*2=67" 
+        analyze = Analyze(equation)
+
+        isOk = analyze.IsValidate()
+
+        self.assertFalse(isOk)
+
+
+
+    def test_equal_is_missing(self) :
+        equation = "87x*867"
+        analyze = Analyze(equation)
+
+        isOk = analyze.IsValidate()
+
+        self.assertFalse(isOk)
+
+
+
+    def test_equal_is_wrong_place(self) : 
+        equationFirst = "=92x+87"
+        analyzeFirst = Analyze(equationFirst)
+        equationSecond = "92x+87="
+        analyzeSecond = Analyze(equationSecond)
+        equationThird = "92x+87+=3"
+        analyzeThird = Analyze(equationThird)
+        equationFourth = "92x+87-=3"
+        analyzeFourth = Analyze(equationFourth)
+        equationFifth = "92x+87*=3"
+        analyzeFifth = Analyze(equationFifth)
+        equationSixth = "92x+87/=3"
+        analyzeSixth = Analyze(equationSixth)
+
+        isOkFirst = analyzeFirst.IsValidate()
+        isOkSecond = analyzeSecond.IsValidate()
+        isOkThird = analyzeThird.IsValidate()
+        isOkFourth = analyzeFourth.IsValidate()
+        isOkFifth = analyzeFifth.IsValidate()
+        isOkSixth = analyzeSixth.IsValidate()
+
+        self.assertFalse(isOkFirst)
+        self.assertFalse(isOkSecond)
+        self.assertFalse(isOkThird)
+        self.assertFalse(isOkFourth)
+        self.assertFalse(isOkFifth)
+        self.assertFalse(isOkSixth)
+
+
+    def test_unknown_in_left_part(self) :
+        equation = "32=87x*867"
+        analyze = Analyze(equation)
+
+        isOk = analyze.IsValidate()
+
+        self.assertFalse(isOk)
