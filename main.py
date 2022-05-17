b8147da37276b67bdbd8ba5e164f60ffed2a8149
Pierre-Louis Serré <plouiserre@gmail.com>
2022-05-17 08:59:14 +0200

    complete main.py

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ if isValidate :
     for part in input.equation.parts : 
         print("Text of part %s of the equation " % part.text)
         #TODO ici il faut améliorer le code
-        print("Group of part %s of the equation " % part.signs_numbers)
-    
-
+        print("Number of sign_numbers in this part %d of the equation " % len(part.signs_numbers))
+        for sign_number in part.signs_numbers : 
+            print("Sign Number group %s " %sign_number.text)
+            print("priority %d "%sign_number.priority)
+            print("position %d "%sign_number.position)
+            print("order %d "%sign_number.order)
 else :
     print("Equation is not validate\n")
     print("Program is down.")
