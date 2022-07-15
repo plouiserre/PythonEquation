f62ce985ba6fe307bb8bf39a799a635da46b336f
Pierre-Louis Serré <plouiserre@gmail.com>
2022-07-15 16:36:13 +0200

    finish main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from analyze import Analyze
+from rewrite import Rewrite
+from equation import Equation
 
 
 print("Which equation do you want resolve?")
-equation = input()
-input = Analyze(equation)
-isValidate = input.is_validate()
+text_equation = input()
+analyze = Analyze(text_equation)
+rewrite = Rewrite()
+eq = Equation(text_equation, analyze, rewrite)
+
+isValidate = analyze.is_validate()
 if isValidate :
     print("Equation is validate\n")
-    input.identification()
-    for part in input.equation.parts : 
-        print("Text of part %s of the equation " % part.text)
-        #TODO ici il faut améliorer le code
-        print("Number of sign_numbers in this part %d of the equation " % len(part.signs_numbers))
-        for sign_number in part.signs_numbers : 
-            print("Sign Number group %s " %sign_number.text)
-            print("priority %d "%sign_number.priority)
-            print("position %d "%sign_number.position)
-            print("order %d "%sign_number.order)
+    eq.resolve()
+    print("Solution is %f" %eq.unknown_value)
 else :
     print("Equation is not validate\n")
     print("Program is down.")
